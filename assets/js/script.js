31ac8235fbf60dd3abb7e810a7994720352cf941
Gabr1313 <76425475+Gabr1313@users.noreply.github.com>
2021-01-01 13:12:05 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,19 @@ ordineAlfabeticoCognome= function (a,b){
 
 numeroIntValido = function(num, Classe){
     //controllo che il numero sia valido, considerando pure gli studenti rimossi
-    x=0;
+    var x=0;
     for (e of Classe) if (e.valorePredefinito(Classe)===-1) x++;
     if (num>0&&num<=(Classe.length-x)) return num;
     return 0;
 }
 
-CambiaPosizione= function(studente){
+CambiaPosizione= function(studente,Classe){
     //se ho già cliccato, scambio
     if (click1) {
         click1=false;
 
-        for (i=0;i<gruppi.length;i++){
-            for (j=0;j<gruppi[i].length;j++){
+        for (var i=0;i<gruppi.length;i++){
+            for (var j=0;j<gruppi[i].length;j++){
                 if (gruppi[i][j]===studente) {
                     IndiceStudente2=[i,j];
                     document.getElementsByTagName("li")[i].childNodes[j+1].style.backgroundColor = "abcdef";
@@ -27,20 +27,19 @@ CambiaPosizione= function(studente){
             }
         }
 
-        studenteAppoggio=gruppi[IndiceStudente1[0]][IndiceStudente1[1]];
+        var studenteAppoggio=gruppi[IndiceStudente1[0]][IndiceStudente1[1]];
         gruppi[IndiceStudente1[0]][IndiceStudente1[1]]=gruppi[IndiceStudente2[0]][IndiceStudente2[1]];
         gruppi[IndiceStudente2[0]][IndiceStudente2[1]]=studenteAppoggio;
 
-        for (gruppo of gruppi) 
-        gruppo.sort(ordineAlfabeticoCognome);
-        setTimeout(function(){stampaGruppi(gruppi,"out")}, 150);
+        for (gruppo of gruppi) gruppo.sort(ordineAlfabeticoCognome);
+        setTimeout(function(){stampaGruppi(gruppi,1,Classe)}, 150);
     }
 
     //se non ho già cliccato, evidenzio il primo
     else{
         click1=true;
-        for (i=0;i<gruppi.length;i++){
-            for (j=0;j<gruppi[i].length;j++){
+        for (var i=0;i<gruppi.length;i++){
+            for (var j=0;j<gruppi[i].length;j++){
                 if (gruppi[i][j]===studente) {
                     IndiceStudente1=[i,j];
                     document.getElementsByTagName("li")[i].childNodes[j+1].style.backgroundColor = "abcdef";
@@ -50,59 +49,71 @@ CambiaPosizione= function(studente){
     }
 }
 
-creaGruppi= function(elencoDinamico,numeroInt,Classe){
-    //controllo che non ci siano troppi studenti nella stessa classe pre-inseriti
-    length=elencoDinamico.length;
+creaGruppi= function(elencoDinamico,numeroInt,Classe,tipo){
+    var length=elencoDinamico.length;
 
     //rimuove gli studenti da ElencoDinamico
-    for (i=0;i<elencoDinamico.length;i++){
-        g=elencoDinamico[i].valorePredefinito(Classe);
+    for (var i=0;i<elencoDinamico.length;i++){
+        var g=elencoDinamico[i].valorePredefinito(Classe);
         if (g<0) length--;
     }
 
-    scarto=length%numeroInt;
-    NumStudenti=parseInt(length/numeroInt);
-    gruppi=[];
+    var scarto=length%numeroInt;
+    var NumStudenti=parseInt(length/numeroInt);
 
     //genera i gruppi vuoti
-    for (i=0;i<numeroInt;i++){
-        if (i<scarto) numeroStudenti=NumStudenti+1;
-        else numeroStudenti=NumStudenti;
-        gruppo=[];
+    for (var i=0;i<numeroInt;i++){
+        if (tipo){
+            if (i<scarto) numeroStudenti=NumStudenti+1;
+            else numeroStudenti=NumStudenti;
+        }else{
+            numeroStudenti=1;
+        }
+        var gruppo=[];
         gruppo.length=numeroStudenti;
         gruppi.push(gruppo);
     }
     
-    //aggiunge gli studenti ai determinati gruppi
-    for (i=0;i<elencoDinamico.length;i++){
-        g=elencoDinamico[i].valorePredefinito(Classe);
-        if (g>0&&g-1<gruppi.length){
-            for (j=0;j<gruppi[g-1].length;j++){
-                if (gruppi[g-1][j]===undefined) {
-                gruppi[g-1][j]=elencoDinamico[i];
-                break;
+    if (tipo){
+        //aggiunge gli studenti ai gruppi pre-determinati
+        for (var i=0;i<elencoDinamico.length;i++){
+            var g=elencoDinamico[i].valorePredefinito(Classe);
+            if (g>0 && g-1<gruppi.length){
+                for (j=0;j<gruppi[g-1].length;j++){
+                    if (gruppi[g-1][j]===undefined) {
+                    gruppi[g-1][j]=elencoDinamico[i];
+                    break;
+                    }
                 }
             }
         }
     }
+    
 
     //rimuove gli studenti da non inserire a caso
-    t=0
-    elencoSupporto=Object.assign([], elencoDinamico);
-    for (x=0;x<elencoSupporto.length;x++){
-        g=elencoSupporto[x].valorePredefinito(Classe);
-        if (g!=0){
-            elencoDinamico.splice(t,1);
-            t--;
+    var t=0
+    var elencoSupporto=Object.assign([], elencoDinamico);
+    for (var x=0;x<elencoSupporto.length;x++){
+        var g=elencoSupporto[x].valorePredefinito(Classe);
+        if (tipo){
+            if (g!=0){
+                elencoDinamico.splice(t,1);
+                t--;
+            }  
+        }else{
+            if (g<0){
+                elencoDinamico.splice(t,1);
+                t--;
+            } 
         }
         t++;
     }
 
     //riempio i gruppi
-    for (i=0;i<gruppi.length;i++){
-        for (j=0;j<gruppi[i].length;j++){
+    for (var i=0;i<gruppi.length;i++){
+        for (var j=0;j<gruppi[i].length;j++){
             if (gruppi[i][j]===undefined){
-                StudenteIndice=Math.floor(Math.random() * elencoDinamico.length);
+                var StudenteIndice=Math.floor(Math.random() * elencoDinamico.length);
                 gruppi[i][j]=elencoDinamico.splice(StudenteIndice,1)[0];
             }
         }
@@ -113,40 +124,57 @@ creaGruppi= function(elencoDinamico,numeroInt,Classe){
     return gruppi;
 }
 
-stampaGruppi= function(gruppi){
+stampaGruppi= function(gruppi,tipo){
     //elimino il contenuto di form
-    form=document.getElementsByTagName("form")[0];
+    var form=document.getElementsByTagName("form")[0];
     form.innerHTML=``;
 
     //creo section
-    section=document.getElementById("out");
-    section.innerHTML=`
-    <h2>I gruppi estratti sono:</h2>
-    <ol id="outgruppo"></ol>
-    `;
-    out=document.getElementById("outgruppo");
-    for (i=0;i<gruppi.length;i++){
-        li=document.createElement("li");
-        h3=document.createElement("h3")
-        h3.innerHTML="Gruppo "+(i+1)+ ":";
-        li.appendChild(h3);
-        for (e of gruppi[i]) li.appendChild(e.toHTMLout());
+    var section=document.getElementById("out");
+    if (tipo){
+        section.innerHTML=`
+        <h2>Gruppi estratti:</h2>
+        <ol id="outgruppo"></ol>
+        `;
+    }else{
+        if (gruppi.length===1){
+            section.innerHTML=`
+            <h2>Studente estratto:</h2>
+            <ol id="outgruppo"></ol>
+        `;
+        }else {
+            section.innerHTML=`
+            <h2>Studenti estratti:</h2>
+            <ol id="outgruppo"></ol>
+        `;
+        }
+    }
+    
+    //stampo i gruppi
+    var out=document.getElementById("outgruppo");
+    for (var i=0;i<gruppi.length;i++){
+        var li=document.createElement("li");
+        if (tipo){
+            var h3=document.createElement("h3")
+            h3.innerHTML="Gruppo "+(i+1)+ ":";
+            li.appendChild(h3);
+        }
+        for (e of gruppi[i]) li.appendChild(e.toHTMLout(tipo));
         out.appendChild(li);
     }
 
     //creo il bottone nel footer
-    buttonReset=document.createElement("button");
+    var footer= document.getElementsByTagName("footer")[0];
+    footer.innerHTML=``;
+    var buttonReset=document.createElement("button");
     buttonReset.innerHTML= "Estrai nuovamente";
     buttonReset.onclick = () => window.location.reload();
-    footer= document.getElementsByTagName("footer")[0];
-    footer.innerHTML=``;
-    footer.appendChild(buttonReset);
-    
+    footer.appendChild(buttonReset);  
 }
 
 stampaInizio= function(Classe){
     //stampa l'elenco di studenti da cui selezionare i gruppi predefiniti
-    out=document.getElementById("inizio");
+    var out=document.getElementById("inizio");
     for (e of Classe) out.appendChild(e.toHTMLinizio());
 }
 
@@ -156,14 +184,14 @@ controllaGruppi = function (gruppi){
     return true;
 }
 
-generaGruppi = function(Classe){
-    //quello che succede a cliccare il bottone
+generaGruppi = function(Classe,tipo){
+    //quello che succede a cliccare il bottone "Crea gruppi"
     numeroInt= numeroIntValido(parseInt(document.getElementsByTagName("input")[0].value),Classe);
     if (!numeroInt) alert("Numero di gruppi non valido.");
     else{
         elencoDinamico=Object.assign([], Classe);
-        gruppi=creaGruppi(elencoDinamico,numeroInt, Classe);
-        if (controllaGruppi(gruppi)) stampaGruppi(gruppi);
+        var gruppi=creaGruppi(elencoDinamico,numeroInt, Classe,tipo);
+        if (controllaGruppi(gruppi)) stampaGruppi(gruppi,tipo,Classe);
         else alert("Troppe persone in un gruppo o gruppo non esistente");
     }
 }
@@ -172,22 +200,22 @@ generaGruppi = function(Classe){
 function studente (nome,cognome){
     this.nome=nome;
     this.cognome=cognome;
-    this.toHTMLout = function (){
+    this.toHTMLout = function (type){
         //crea il tag dello studente nella stampa dei gruppi
-        p = document.createElement("p");
+        var p = document.createElement("p");
         p.innerHTML = this.nome + " " + this.cognome;
-        p.onclick = () => CambiaPosizione(this);
+        if (type) p.onclick = () => CambiaPosizione(this);
         return p;
     }
     this.toHTMLinizio = function (){
         //crea il tag dell'elenco iniziale
-        div = document.createElement("div");
+        var div = document.createElement("div");
 
-        input = document.createElement("input");
+        var input = document.createElement("input");
         input.type= "number";
         input.className= "rimuovi";
 
-        p = document.createElement("p");
+        var p = document.createElement("p");
         p.innerHTML = this.nome + " " + this.cognome;
         
         div.appendChild(input);
@@ -196,10 +224,10 @@ function studente (nome,cognome){
     }
     this.valorePredefinito = function (Classe){
         //legge il valore che gli do nell'elenco iniziale 
-        daRimuovere= document.getElementsByClassName("rimuovi");
-        for (i=0;i<Classe.length;i++){
+        var daRimuovere= document.getElementsByClassName("rimuovi");
+        for (var i=0;i<Classe.length;i++){
             if (Classe[i]==this) {
-                valore=document.getElementsByClassName("rimuovi")[i].value;
+                var valore=document.getElementsByClassName("rimuovi")[i].value;
                 if (valore=="") valore=0;
                 return parseInt(valore);
             }
@@ -207,7 +235,6 @@ function studente (nome,cognome){
         return 0;
     }
 }
-
 
 //creo la classe classe
 var quarta_asa = [
@@ -239,16 +266,21 @@ var quarta_asa = [
 ].sort(ordineAlfabeticoCognome);
 
 //variabili globali che servono nella funzione CambiaPosizione()
-click1=false;
-IndiceStudente1=[];
-IndiceStudente2=[];
+var click1=false;
+var IndiceStudente1=[];
+var IndiceStudente2=[];
+var gruppi=[];
 
 //previene che premendo invio si ricarichi la pagina, ma anzi, fa come se si cliccasse sul bottone
 var form = document.getElementsByTagName("form")[0];
 form.onsubmit = (event) => { event.preventDefault() }
 
-//cosa succede quando clicco sul bottone nella pagina
-document.getElementsByTagName("button")[0].onclick = () => generaGruppi(quarta_asa);
+//cosa succede quando clicco sui bottoni
+document.getElementsByClassName("button0")[0].onclick = () => generaGruppi(quarta_asa,0,true);
+document.getElementsByClassName("button0")[1].onclick = () => generaGruppi(quarta_asa,1,true);
+
+//metto come valore predefinito al numero di gruppi 1
+document.getElementsByTagName("input")[0].valueAsNumber = 1; 
 
 //stampo l'elenco iniziale
 stampaInizio(quarta_asa);
